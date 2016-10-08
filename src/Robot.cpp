d77b112d35cdcc8c6954c95a6c2014938e21e53e
Daniel Pham <danieltapham@gmail.com>
2016-10-08 15:10:17 -0400

    first prototype

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,13 +1,10 @@
 #include "WPILib.h"
-#include "AHRS.h"
 #include <math.h>
 #include <memory>
 
-#define PI 3.14159265
-
 using std::shared_ptr;
 
-class Robot: public IterativeRobot
+class Robot: public IterativeRobot, public PIDOutput
 {
 private:
 	LiveWindow *lw = LiveWindow::GetInstance();
@@ -16,14 +13,35 @@ private:
 	const std::string autoNameCustom = "My Auto";
 	std::string autoSelected;
 
-	void RobotInit()
+	CANTalon *leftdrive1 = new CANTalon(0);
+	CANTalon *leftdrive2= new CANTalon(1);
+	CANTalon *rightdrive1 = new CANTalon(2);
+	CANTalon *rightdrive2= new CANTalon(3);
+	CANTalon *shooterMotor1 = new CANTalon(4);
+	CANTalon *shooterMotor2 = new CANTalon(5);
+	CANTalon *intakeMotor = new CANTalon(6);
+
+	RobotDrive *drive = new RobotDrive(leftdrive2, leftdrive1, rightdrive2,
+			rightdrive1);
+
+	Joystick *controlstick = new Joystick(2);
+	Joystick *leftjoystick = new Joystick(0);
+	Joystick *rightjoystick = new Joystick(1);
+
+  PowerDistributionPanel *pdp = new PowerDistributionPanel(0);
+
+
+	void RobotInit() override
 	{
 		chooser = new SendableChooser();
 		chooser->AddDefault(autoNameDefault, (void*)&autoNameDefault);
 		chooser->AddObject(autoNameCustom, (void*)&autoNameCustom);
 		SmartDashboard::PutData("Auto Modes", chooser);
 	}
-
+	double rotateRate = 0;
+	void PIDWrite(float output) { // Implement PIDOutput
+		rotateRate = output;
+	}
 
 	/**
 	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
@@ -58,17 +76,56 @@ private:
 
 	void TeleopInit()
 	{
-
+		drive->SetExpiration(200000);
+		drive->SetSafetyEnabled(false);
+		liftdown->Set(false);
 	}
+
+  double speed;
+  Timer *timer1 = new Timer();
+	Timer *timre2 = new Timer();
 
 	void TeleopPeriodic()
 	{
-
+		speed = leftJoystick->GetRawButton(1) ? 1 : 0.5;
+		drive->TankDrive(leftjoystick->GetY() * speed, rightjoystick->GetY() * speed);
+		if(controlstick->GetRawButton(2))
+		{
+			timer1->Reset();
+			timer1->Start();
+			PickBall();
+		}
+		if(controlstick->GetRawButton(1))
+		{
+			timer2->Reset();
+			timer2->Start();
+			ShootBall();
+		}
+		if(timer1->Get() >= 1)
+		{
+			intakeMotor->Set(0);
+		}
+		if(timer2->Get() >= .5)
+		{
+			shooterMotor1->Set(0);
+			shooterMotor2->Set(0);
+		}
 	}
 
 	void TestPeriodic()
 	{
 		lw->Run();
+	}
+
+	void PickBall()
+	{
+		intakeMotor->Set(1);
+	}
+
+	void ShootBall()
+	{
+		shooterMotor1->Set(1); // maybe reverse one motor if needed
+		shooterMotor2->Set(1);
 	}
 };
 
